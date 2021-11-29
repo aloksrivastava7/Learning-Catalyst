4bb8d6842552b0e45ee530ca2bb5d091e19310dd
Alok Srivastava <awwwssmme@gmail.com>
2021-11-30 01:08:25 +0530

    updating Validations

--- js/signin_main.js
+++ js/signin_main.js
@@ -1,5 +1,36 @@
+
+function verifyPassword() {
+  var pw = document.getElementById("pass").value;
+  //check empty password field
+  if(pw == "") {
+     document.getElementById("message").innerHTML = "**Fill the password please!";
+     return false;
+  }
+
+ //minimum password length validation
+  if(pw.length < 5) {
+     document.getElementById("message").innerHTML = "**Password length must be atleast 5 characters";
+     return false;
+  }
+
+//maximum length of password validation
+  if(pw.length > 15) {
+     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";
+     return false;
+  } else {
+     document.getElementById("message").innerHTML = "**Okay";
+  }
+}
+function Validate() {
+        var password = document.getElementById("pass").value;
+        var confirmPassword = document.getElementById("re_pass").value;
+        if (password != confirmPassword) {
+            document.getElementById("message").innerHTML = "**Passwords Don't Match";
+            return false;
+        }
+        return true;
+    }
+
 document.getElementById("login-form").addEventListener("submit",(event)=> {
     event.preventDefault()
 })
-
-
